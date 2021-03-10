8ac2c508378f1a4b502d09d47f321df71e37bd4d
henimex <49072861+henimex@users.noreply.github.com>
2021-03-11 00:34:42 +0300

    Data Received

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,23 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/models/product';
+import { HttpClient } from '@angular/common/http';
+import { ProductResponseModel } from 'src/app/models/productResponseModel';
 
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
-  styleUrls: ['./product.component.css']
+  styleUrls: ['./product.component.css'],
 })
 export class ProductComponent implements OnInit {
+  // product1 = { productId: 1, productName: 'Laptop', categoryId: 1, unitPrice: 5, unitsInStock:12 }
+  // products: Product[] = [this.product1]
 
-  product1 = { productId: 1, productName: 'Laptop', categoryId: 1, unitPrice: 5 }
-  product2 = { productId: 2, productName: 'Desktop Pc', categoryId: 3, unitPrice: 15 }
-  product3 = { productId: 3, productName: 'Gaming Pc', categoryId: 3, unitPrice: 25 }
-  product4 = { productId: 4, productName: 'HeadPhones', categoryId: 4, unitPrice: 35 }
-  product5 = { productId: 5, productName: 'Door', categoryId: 2, unitPrice: 43 }
-
-  products = [this.product1, this.product2, this.product3, this.product4, this.product5]
-
-  constructor() { }
+  products: Product[] = [];
+  apiUrl = 'https://localhost:44376/api/products/getall';
+  constructor(private httpClient: HttpClient) {}
 
   ngOnInit(): void {
+    this.getProducts();
   }
 
+  getProducts() {
+    this.httpClient
+      .get<ProductResponseModel>(this.apiUrl)
+      .subscribe((response) => {
+        this.products = response.data
+      });
+  }
 }
