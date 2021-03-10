0016f89be8ea0c40910a34e39d1f983f033c56ac
henimex <49072861+henimex@users.noreply.github.com>
2021-03-11 02:15:45 +0300

    Categoreies Listed

---
+++ src/app/services/category.service.ts
@@ -0,0 +1,16 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { CategoryResponseModel } from '../models/categoryResponseModel';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CategoryService {
+  apiUrl = 'https://localhost:44376/api/categories/getall';
+  constructor(private httpClient: HttpClient) {}
+
+  getCategories(): Observable<CategoryResponseModel> {
+    return this.httpClient.get<CategoryResponseModel>(this.apiUrl);
+  }
+}
