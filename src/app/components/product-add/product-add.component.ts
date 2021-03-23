1bf0b8435497e7dd9dd2d8c923b09a6340456426
henimex <49072861+henimex@users.noreply.github.com>
2021-03-24 02:41:26 +0300

    Add-Response Error

--- src/app/components/product-add/product-add.component.ts
+++ src/app/components/product-add/product-add.component.ts
@@ -39,9 +39,14 @@ export class ProductAddComponent implements OnInit {
     if (this.productAddForm.valid) {
       let productModel = Object.assign({}, this.productAddForm.value);
       this.productService.add(productModel).subscribe((response) => {
-        console.log(productModel);
-        console.log(response);
-        this.toastrService.success('Ürün Eklendi', 'Başarılı');
+        this.toastrService.success(response.message, 'Başarılı');
+      },responseError=>{
+        if (responseError.error.Errors.length > 0) {
+          console.log(responseError.error.Errors)
+          for (let i = 0; i < responseError.error.Errors.length; i++) {
+            this.toastrService.error(responseError.error.Errors[i].ErrorMessage,"Doğrulama Hatası")
+          }
+        }
       });
     } else {
       this.toastrService.error(
--- src/app/models/responseModel.ts
+++ src/app/models/responseModel.ts
@@ -1,4 +1,4 @@
 export interface ResponseModel {
   success: boolean;
-  messsage: string;
+  message: string;
 }
